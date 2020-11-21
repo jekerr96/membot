3e76431585a032d4cb8706459a5f753acc1235df
Евгений <jekerr96@gmail.com>
2020-11-21 17:52:43 +0700

    * перенес добавление юзера в метод, который вызывается всегда при получении сообщения

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({extended: true}));
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
 const VkBot = require("node-vk-bot-api");
 const User = require("./app/models/user");
 const Mem = require("./app/models/mem");
@@ -27,8 +33,16 @@ const Messages = require("./app/messages");
     let audioModel = new Audio();
     let bundleModel = new Bundle();
 
+    global.vkBot.use(async (ctx, next) => {
+        try {
+            await userModel.addUserIfNotExist(ctx.message.peer_id);
+            await next();
+        } catch (e) {
+            console.error(e);
+        }
+    });
+
     global.vkBot.command(Messages.GIVE_MEM, async (ctx) => {
-        await userModel.addUserIfNotExist(ctx.message.peer_id);
         let randomMem = await memModel.getRandomMem();
         ctx.reply("Держи мемчик", randomMem.getCode(), defaultKeyboard);
     });
@@ -40,7 +54,6 @@ const Messages = require("./app/messages");
     });
 
     global.vkBot.command(Messages.GIVE_AUDIO, async (ctx) => {
-        await userModel.addUserIfNotExist(ctx.message.peer_id);
         let randomAudio = await audioModel.getRandomAudio();
         ctx.reply("Делай громче", randomAudio.getCode(), defaultKeyboard);
     });
@@ -71,8 +84,37 @@ const Messages = require("./app/messages");
     app.set("view engine", "pug");
     app.disable('x-powered-by');
 
-    app.get("/", (req, res) => {
-        res.render("index", {title: "membot", message: "hello world", pageType: "main"});
+    app.get("/", async (req, res) => {
+        let bundle = await bundleModel.getAllElements();
+
+        res.render("index", {title: "membot", message: "hello world", pageType: "main", bundles: bundle});
+    });
+
+    app.post("/bot/send-bundle-all/", async (req, res) => {
+        let usersId = await userModel.getArrayUsersId();
+        let msg = req.body.bundleText;
+        let bundle = await bundleModel.getElementById(req.body.bundleId);
+
+        if (!bundle) {
+            req.send({
+                success: false
+            });
+
+            return;
+        }
+
+        let codes = await bundle.getCodesForSend();
+
+        usersId.forEach(userId => {
+            //какого то хрена сообщения не отправляются если делать это синхронно
+            setTimeout(() => {
+                global.vkBot.sendMessage(userId, msg, codes, defaultKeyboard);
+            }, 1);
+        });
+
+        res.send({
+            success: true,
+        });
     });
 
     app.listen(port, () => {
